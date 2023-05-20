e6c91304ae3676030fe08ee83ed2be951866a64f
ahad1033 <a.ahad2232@gmail.com>
2023-05-20 17:55:02 +0600

    footer added

--- src/pages/home/Home/Home.jsx
+++ src/pages/home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Footer from "../../shared/footer/Footer";
 import Banner from "../banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Footer />
         </div>
     );
 };
---
+++ src/pages/shared/footer/Footer.jsx
@@ -0,0 +1,43 @@
+
+
+const Footer = () => {
+    return (
+        <div className="grid lg:grid-cols-4 md:grid-cols 1 my-5 p-10">
+            <div className="mt-4">
+                <h2 className="text-2xl font-bold">Our Upcoming Toys</h2>
+                <ul>Sports Car</ul>
+                <ul>Cooking Toys</ul>
+                <ul>Robot Toys</ul>
+                <ul>Disney Dolls</ul>
+                <ul>Musical Toys</ul>
+            </div>
+            <div className="mt-4">
+                <h2 className="text-2xl font-bold">About Us</h2>
+                <ul>Contact us</ul>
+                <ul>Company profile</ul>
+                <ul>Advertise with us</ul>
+                <ul>Company support</ul>
+                <ul>Meet the team</ul>
+                <ul>Cookie and privacy</ul>
+
+            </div>
+            <div className="mt-4">
+                <h2 className="text-2xl font-bold">Customer Account</h2>
+
+                <ul>Sign In</ul>
+                <ul>Create New Account</ul>
+                <ul>Having Trouble?</ul>
+                <ul>My Wish List</ul>
+                <ul>Recently Viewed</ul>
+
+            </div>
+            <div className="mt-4">
+                <h2 className="text-2xl font-bold">Need Help?</h2>
+                <p><span className="font-semibold">Call us:</span> +9874 666 999</p>
+                <p><span className="font-semibold">Email:</span> toyland@gmail.com</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
